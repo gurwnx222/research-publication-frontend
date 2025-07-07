76045a08454aea49c99235488618d2fca15ef719
Shivam Shori <shivamshori08@gmail.com>
2025-07-07 14:17:19 +0530

    replaced redirect logic

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -15,7 +15,7 @@ const ProtectedRoute = ({ children }) => {
 
   // Redirect to login if not authenticated
   if (!isAuthenticated) {
-    return <Navigate to="/admin-login" replace />;
+    return <Navigate to="/" replace />;
   }
 
   // Render the protected component if authenticated
